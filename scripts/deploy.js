12541acffc57ae411edd8c44820afcf8ddd8a6b2
Shravani <shrvnsrvstv@gmail.com>
2024-05-03 01:51:57 -0700

    Fixed deployment issue

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,36 @@
+// taskmanager.js
+
 async function main() {
+  // To get the wallet address of current deployer
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log(
+    "Account balance:",
+    await deployer.provider.getBalance(deployer.address)
+  );
+
+  // To deploy the contract
   const TaskManager = await ethers.getContractFactory("TaskManager");
+  console.log("Deploying TaskManager...");
   const taskManager = await TaskManager.deploy();
 
-  console.log("TaskManager deployed to:", taskManager.address);
+  // await taskManager.waitForDeployment();
+
+  // To get the deployed contract target address
+  const address = await taskManager.target;
+  console.log("TaskManager deployed to:", address);
+
+  // Optionally, you can interact with the deployed contract here
+  // For example:
+  const taskCount = await taskManager.taskCount();
+  console.log("Initial task count:", taskCount);
 }
 
 main()
   .then(() => process.exit(0))
   .catch(error => {
-      console.error(error);
-      process.exit(1);
+    console.error(error);
+    process.exit(1);
   });
